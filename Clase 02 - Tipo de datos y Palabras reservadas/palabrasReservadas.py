f047710c360fcbc45043f6ec6fe474316f05d6c9
José Ojeda <jojeda5171@uta.edu.ec>
2022-08-18 21:38:01 -0500

    Tipo de datos

---
+++ Clase 02 - Tipo de datos y Palabras reservadas/palabrasReservadas.py
@@ -0,0 +1,30 @@
+#True -> valor de verdad
+#False -> valor de negacion
+#and -> operador logico y
+#not -> negar variables
+#or -> disyuncion
+#as -> renombrar variables
+#assert -> nos ayudaria a depurar el codigo
+#async y await -> funcionalidades asincronicas
+#break -> terminar con un ciclo
+#Continue -> nos permite saltar una iteracion
+#class -> realizar objetos
+#def -> crear funciones
+#del -> eliminar un objeto
+#elif y else -> nos permite declarar una condicion
+#except -> encapsula una exepcion
+#finally -> encapsula una porcion de codigo
+#for -> permite realizar un bucle
+#from -> ayuda en los importes
+#global -> declara una variable de forma global
+#if -> nos permite tener condiciones
+#import -> nos permite realizar importaciones de diferentes modulos
+#in -> nos permite conocer si un valor existe en una lista
+#is -> nos permite saber si dos objetos son iguales, nos retorna un valor de True o False
+#pass -> nos define una funcion que aun no esta terminada
+#return -> retornar los valores de una funcion 
+#Try -> encapsulamos el codigo
+#while -> bucle
+#with -> permite un mejor manejo de excepciones
+#yield -> el uso similar al de un return 
+
